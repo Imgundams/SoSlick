11747cac43e8f152d39debb2d32bccf541575912
Administrator <alex.rimmer@qa.com>
2017-06-30 12:41:14 +0100

    Fully working

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -8,6 +8,7 @@ import scala.util.{Failure, Success}
 
 
 object Main extends App {
+	// Go to the Terminal and type 'sbt' then once done type 'run' to run the code
 
 	// The config string refers to mysqlDB that we defined in application.conf
 	val db = Database.forConfig("so_slick")
@@ -22,9 +23,9 @@ object Main extends App {
 	val initPeopleCmd = DBIO.seq(peopleTable.schema.create)
 
 
-	//dropDB
-	runQueryUpdate(3, "Scala James", "Whiteley", 22)
+	//runQueryUpdate(3, "Scala James", "Whiteley", 22)
 
+	averagePeoplesAge
 	print("Yoooooo")
 
 
@@ -60,7 +61,25 @@ object Main extends App {
 			val query = peopleTable ++= Seq(
 				(1, "Jack", "Wood", 36),
 				(2, "Tim", "Brown", 24),
-				(3, "James", "Scala", 22)
+				(3, "James", "Scala", 22),
+				(4, "Jim", "Smith", 36),
+				(5, "Dave", "Smith", 24),
+				(6, "Dug", "Willow", 36),
+				(7, "Thomas", "Dover", 24),
+				(8, "Jack", "Wood", 36),
+				(9, "Vain", "Brown", 24),
+				(10, "Jack", "Wood", 36),
+				(11, "Tim", "Brown", 24),
+				(12, "Jack", "Wood", 36),
+				(13, "Tim", "Not", 24),
+				(14, "Jack", "Wood", 36),
+				(15, "Tim", "What", 24),
+				(16, "Jack", "Wood", 36),
+				(17, "Tim", "Will", 24),
+				(18, "Jack", "Wood", 36),
+				(19, "Tim", "Hello", 24),
+				(20, "Jack", "Wood", 36),
+				(21, "Tim", "Brown", 24)
 			) // insert into `PEOPLE` (`PER_FNAME`,`PER_LNAME`,`PER_AGE`)  values (?,?,?)
 			println(query.statements.head) // would print out the query one line up
 			db.run(query)
@@ -81,6 +100,33 @@ object Main extends App {
 		}
 		Await.result(queryFuture, Duration.Inf).andThen {
 			case Success(_) => db.close() //cleanup DB connection
+			case Failure(error) => println("Listing people failed due to: " + error.getMessage)
+		}
+	}
+
+	def countPeople = {
+		var count:Int =0
+		val queryFuture = Future {
+			// simple query that selects everything from People and prints them out
+			db.run(peopleTable.result).map(_.foreach {p => p ;count+=1})
+		}
+		Await.result(queryFuture, Duration.Inf).andThen {
+			case Success(_) => println("The people count of the database is "+ count)
+				db.close() //cleanup DB connection
+			case Failure(error) => println("Listing people failed due to: " + error.getMessage)
+		}
+	}
+
+	def averagePeoplesAge = {
+		var count:Int =0
+		var averageAge:Int = 0
+		val queryFuture = Future {
+			// simple query that selects everything from People and prints them out
+			db.run(peopleTable.result).map(_.foreach {p => averageAge+=p._4 ;count+=1})
+		}
+		Await.result(queryFuture, Duration.Inf).andThen {
+			case Success(_) => println("The average age of people in the database is "+ averageAge/count)
+				db.close() //cleanup DB connection
 			case Failure(error) => println("Listing people failed due to: " + error.getMessage)
 		}
 	}
